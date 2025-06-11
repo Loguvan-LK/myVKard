3dde7bf1f8a14bc525a5c923a54954ec7ef93c76
teammonarch <teammonarch.ai@gmail.com>
2025-06-11 12:00:40 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,27 +5,29 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: 'components', replacement: path.resolve(__dirname, 'src/components') },
-      { find: 'assets', replacement: path.resolve(__dirname, 'src/assets') },
-      { find: 'pages', replacement: path.resolve(__dirname, 'src/pages') },
-      { find: 'utils', replacement: path.resolve(__dirname, 'src/utils') },
-      { find: 'hooks', replacement: path.resolve(__dirname, 'src/hooks') },
-      { find: 'context', replacement: path.resolve(__dirname, 'src/context') },
+   alias: 
+    [
+      {find: '@', replacement: path.resolve(__dirname, 'src')},
+      {find: 'components', replacement: path.resolve(__dirname, 'src/components')},
+      {find: 'assets', replacement: path.resolve(__dirname, 'src/assets')},
+      {find: 'pages', replacement: path.resolve(__dirname, 'src/pages')},
+      {find: 'utils', replacement: path.resolve(__dirname, 'src/utils')},
+      {find: 'hooks', replacement: path.resolve(__dirname, 'src/hooks')},
+      {find: 'context', replacement: path.resolve(__dirname, 'src/context')},
     ]
-  },
+  }
+
+  ,
   server: {
     host: true,
     port: 5173,
     cors: true,
     allowedHosts: [
       'localhost:5173',
-      '0107-2405-201-e040-e0e5-a9d5-fae4-f072-a2a1.ngrok-free.app'
     ],
     hmr: {
       protocol: 'wss',
-      host: '0107-2405-201-e040-e0e5-a9d5-fae4-f072-a2a1.ngrok-free.app',
+      host: 'localhost:5173',
     },
   },
 });
